5d8763c12abc928db8c15829c9c1b683fec9a3e1
Fester2004 <61381794+Fester2004@users.noreply.github.com>
2020-04-04 20:45:44 +0300

    add method  __str__

--- Server/App/models.py
+++ Server/App/models.py
@@ -5,3 +5,7 @@ class Article(models.Model):
     author= models.CharField(max_length=22)
     title = models.CharField(max_length=128)
     text = models.TextField()
+
+    def __str__(self):
+        template = f'{self.author} {self.title} {self.text}'
+        return template.format(self)
